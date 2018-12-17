ba41843c9e53d42273828db6a6a81e4d15aa0f89
ArturasKriuckovas <arturas.kriuckovas@gmail.com>
2018-12-17 13:12:06 +0200

    added floating point number support

--- StackTrainingCalculator.java
+++ StackTrainingCalculator.java
@@ -1,45 +1,68 @@
 import java.util.Stack;
+import java.util.ArrayList;
+import java.math.BigDecimal;
 public class StackTrainingCalculator {
+    public static String[] allSymbols = {"+", "-", "/", "*","(", ")"};
     public static String[] operators = {"+", "-", "/", "*"};
     public static void main(String[] args) {
         Stack<String> mStack = new Stack<>();
         String postFix = "232+*";
-        String inFixValue = "1*(2+3)*(8+2)";
-        System.out.println(evaluatePostFix(convertToPostfix(inFixValue)));
+        String inFixValue = "12.1*(2+3)*(8.5+2.1)";
+        System.out.println(evaluatePostFix(convertToPostfix(getListOfValues(inFixValue))));
     }
 
-    public static String evaluatePostFix(String value){
+    public static ArrayList<String> getListOfValues(String expression){
+        ArrayList<String> list = new ArrayList<>();
+        String tempValue = "";
+        for (int i = 0; i < expression.length(); i++){
+            String item = Character.toString(expression.charAt(i));
+            if(checkIfContains(item, allSymbols)){
+                list.add(item);
+            } else {
+                tempValue += item;
+                if(checkIfContains(Character.toString(expression.charAt(i+1)), allSymbols)){
+                    list.add(tempValue);
+                    tempValue = "";
+                }
+            }
+        }
+        return list;
+    }
+
+    public static String evaluatePostFix(ArrayList<String> value){
         Stack<String> mStack = new Stack<>();
-        for (int i = 0; i < value.length(); i++){
-            if(checkIfContains(Character.toString(value.charAt(i)), operators)){
+        for (int i = 0; i < value.size(); i++){
+            if(checkIfContains(value.get(i), operators)){
                 String right = mStack.pop();
                 String left = mStack.pop();
-                mStack.push(evaluate(left, right, Character.toString(value.charAt(i))));
+                mStack.push(evaluate(left, right, value.get(i)));
             } else {
-                mStack.push(Character.toString(value.charAt(i)));
+                mStack.push(value.get(i));
             }
         }
         return mStack.pop();
     }
 
     public static String evaluate(String a, String b, String operator){
-        String result = "";
+        BigDecimal left = new BigDecimal(a);
+        BigDecimal right = new BigDecimal(b);
+        BigDecimal result = new BigDecimal(0);
         switch (operator) {
             case "+":
-                result = String.valueOf(Integer.parseInt(a) + Integer.parseInt(b));
+                result = left.add(right);
                 break;
             case "-":
-                result = String.valueOf(Integer.parseInt(a) - Integer.parseInt(b));
+                result = left.subtract(right);
                 break;
             case "*":
-                result = String.valueOf(Integer.parseInt(a) * Integer.parseInt(b));
+                result = left.multiply(right);
                 break;
             case "/":
-                result = String.valueOf(Integer.parseInt(a) / Integer.parseInt(b));
+                result = left.divide(right);
                 break;
             default: break;            
         }
-        return result;
+        return String.valueOf(result);
     }
 
     static boolean checkIfContains(String c, String[] array) {
@@ -61,19 +84,21 @@ public class StackTrainingCalculator {
         
     }
 
-    static String convertToPostfix(String infix){
+    static ArrayList<String> convertToPostfix(ArrayList<String> infix){
         Stack<String> mStack = new Stack<>();
+        ArrayList<String> list = new ArrayList<>();
         String postFix = "";
-        for (int i = 0; i < infix.length(); i++){
+        for (int i = 0; i < infix.size(); i++){
+            String item = infix.get(i);
+
             boolean isOperator = checkIfContains(item, operators);
-            String item = Character.toString(infix.charAt(i));
             if(!isOperator && !item.equals("(") && !item.equals(")")){
-                postFix += Character.toString(infix.charAt(i));
+                list.add(item);
             } else if (item.equals("(")){
                 mStack.push(item);
             } else if (item.equals(")")){
                 while(!mStack.isEmpty() && !mStack.peek().equals("(")){
-                    postFix += mStack.pop();
+                    list.add(mStack.pop());
                 }
                 mStack.pop();
             } else if (isOperator){
@@ -82,15 +107,16 @@ public class StackTrainingCalculator {
                 } else {
                     while(!mStack.isEmpty() && !mStack.peek().equals("(")
                             && getLevelOfOperator(item) <= getLevelOfOperator(mStack.peek())) {
-                    postFix += mStack.pop();
+                    list.add(mStack.pop());
                     }
                     mStack.push(item);
                 }
             }
         }
         while (!mStack.isEmpty()){
-            postFix += mStack.pop();
+            list.add(mStack.pop());
         }
-        return postFix;
+        return list;
+
     }
 }
